ac171776dc6dc15a794e3c2bec5f4d83a974c734
Jonatan Eckeskog <jonataneckeskog@gmail.com>
2025-09-22 00:08:18 +0200

    Change from class to record

--- app/src/main/java/scrabble/engine/core/DrawResult.java
+++ app/src/main/java/scrabble/engine/core/DrawResult.java
@@ -2,20 +2,5 @@ package scrabble.engine.core;
 
 import java.util.List;
 
-public final class DrawResult {
-    private final Bag newBag;
-    private final List<Tile> drawnTiles;
-
-    DrawResult(Bag bag, List<Tile> drawnTiles) {
-        this.newBag = bag;
-        this.drawnTiles = List.copyOf(drawnTiles);
-    }
-
-    public Bag getNewBag() {
-        return newBag;
-    }
-
-    public List<Tile> getDrawnTiles() {
-        return drawnTiles;
-    }
+public record DrawResult(Bag bag, List<Tile> drawnTiles) {
 }

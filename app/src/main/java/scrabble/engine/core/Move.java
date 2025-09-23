c7805fc2edf8ac73da34c41cf3f1d70b5300a7d7
Jonatan Eckeskog <jonataneckeskog@gmail.com>
2025-09-23 21:17:50 +0200

    Implement Move; integration with core logic still pending

--- app/src/main/java/scrabble/engine/core/Move.java
+++ app/src/main/java/scrabble/engine/core/Move.java
@@ -1,5 +1,39 @@
 package scrabble.engine.core;
 
-public class Move {
+import scrabble.engine.core.Position.Step;
 
+import java.util.Map;
+import java.util.Iterator;
+
+public class Move {
+    private Map<Position, Tile> tilePlacementMap;
+
+    public Move() {
+
+    }
+
+    private Step stepFromMap() {
+        if (tilePlacementMap.size() == 1)
+            return Step.RIGHT;
+
+        Iterator<Map.Entry<Position, Tile>> it = tilePlacementMap.entrySet().iterator();
+
+        if (it.hasNext()) {
+            Map.Entry<Position, Tile> entry1 = it.next();
+            if (it.hasNext()) {
+                Map.Entry<Position, Tile> entry2 = it.next();
+                return entry1.getKey().row() == entry2.getKey().row() ? Step.RIGHT : Step.DOWN;
+            }
+        }
+
+        return Step.RIGHT; // fallback
+    }
+
+    public Map<Position, Tile> getTilePlacemenetMap() {
+        return tilePlacementMap;
+    }
+
+    public Step getStep() {
+        return stepFromMap();
+    }
 }

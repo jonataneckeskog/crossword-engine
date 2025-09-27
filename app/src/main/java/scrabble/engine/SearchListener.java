78f4a7b4f51ed960485263e2947f8bf3341bc23d
Jonatan Eckeskog <jonataneckeskog@gmail.com>
2025-09-28 00:01:22 +0200

    Change name from onUpdate to update to make usage more clear

--- app/src/main/java/scrabble/engine/SearchListener.java
+++ app/src/main/java/scrabble/engine/SearchListener.java
@@ -5,5 +5,5 @@ import scrabble.core.Move;
 import java.util.Map;
 
 public interface SearchListener {
-    void onUpdate(Map<Move, Double> winPercentagesMap);
+    void update(Map<Move, Double> winPercentagesMap);
 }

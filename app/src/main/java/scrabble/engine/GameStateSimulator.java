b3252317b08d9feec22a84124c9a63d76d855b03
Jonatan Eckeskog <jonataneckeskog@gmail.com>
2025-09-27 12:02:52 +0200

    Add GameStateSimulator and MoveGenerator

---
+++ app/src/main/java/scrabble/engine/GameStateSimulator.java
@@ -0,0 +1,13 @@
+package scrabble.engine;
+
+import scrabble.core.GameState;
+import scrabble.core.PlayerView;
+
+import java.util.List;
+
+public class GameStateSimulator {
+    public static List<GameState> generate(PlayerView playerview, int numberOfGameStates) {
+        // TO-DO
+        return null;
+    }
+}
---
+++ app/src/main/java/scrabble/engine/MoveGenerator.java
@@ -0,0 +1,14 @@
+package scrabble.engine;
+
+import scrabble.core.Move;
+import scrabble.core.GameState;
+import scrabble.rules.AdvancedDictionary;
+
+import java.util.List;
+
+public class MoveGenerator {
+    public static List<Move> legalMoves(GameState gameState, AdvancedDictionary dictionary) {
+        // TO-DO: Generate all possible legal moves
+        return null;
+    }
+}

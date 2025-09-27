56aa4e898d27189cc53f0b754f5dedee269085df
Jonatan Eckeskog <jonataneckeskog@gmail.com>
2025-09-27 14:14:15 +0200

    Add option to generate moves from Playerview

--- app/src/main/java/scrabble/engine/MoveGenerator.java
+++ app/src/main/java/scrabble/engine/MoveGenerator.java
@@ -2,12 +2,18 @@ package scrabble.engine;
 
 import scrabble.core.Move;
 import scrabble.core.GameState;
+import scrabble.core.PlayerView;
 import scrabble.rules.AdvancedDictionary;
 
 import java.util.List;
 
 public class MoveGenerator {
     public static List<Move> legalMoves(GameState gameState, AdvancedDictionary dictionary) {
+        // TO-DO: Generate all possible legal moves
+        return null;
+    }
+
+    public static List<Move> legalMoves(PlayerView playerView, AdvancedDictionary dictionary) {
         // TO-DO: Generate all possible legal moves
         return null;
     }

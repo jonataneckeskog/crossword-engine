0d4c407fddc915e2e40fb283308e4ce0cad5bdf8
Jonatan Eckeskog <jonataneckeskog@gmail.com>
2025-09-28 00:00:47 +0200

    Fix so that the interface is implemented correctly

--- app/src/main/java/scrabble/engine/StandardEvaluator.java
+++ app/src/main/java/scrabble/engine/StandardEvaluator.java
@@ -3,11 +3,8 @@ package scrabble.engine;
 import scrabble.core.GameState;
 
 public class StandardEvaluator implements Evaluator {
-
     @Override
-    public int evaluate(GameState gameState) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'evaluate'");
+    public double evaluate(GameState gameState) {
+        return gameState.getScores()[0];
     }
-
 }

e9d891542967a460a92765547623065d19967147
Jonatan Eckeskog <jonataneckeskog@gmail.com>
2025-10-02 23:41:08 +0200

    evaluate now takes how open the board is into account for calculating leave score

--- app/src/main/java/scrabble/engine/StandardEvaluator.java
+++ app/src/main/java/scrabble/engine/StandardEvaluator.java
@@ -3,6 +3,8 @@ package scrabble.engine;
 import java.util.Map;
 
 import scrabble.core.GameState;
+import scrabble.rules.game.*;
+import scrabble.core.components.*;
 
 public class StandardEvaluator implements Evaluator {
     private static final Map<Character, Integer> TILE_UTILITIES = Map.ofEntries(
@@ -11,24 +13,41 @@ public class StandardEvaluator implements Evaluator {
             Map.entry('I', 5), Map.entry('J', 1), Map.entry('K', 2), Map.entry('L', 5),
             Map.entry('M', 4), Map.entry('N', 5), Map.entry('O', 5), Map.entry('P', 4),
             Map.entry('Q', 0), Map.entry('R', 5), Map.entry('S', 7), Map.entry('T', 5),
-            Map.entry('U', 5), Map.entry('V', 2), Map.entry('W', 2), Map.entry('X', 1),
+            Map.entry('U', 5), Map.entry('V', 2), Map.entry('W', 2), Map.entry('X', 5),
             Map.entry('Y', 2), Map.entry('Z', 0), Map.entry('?', 10) // Blank tile
     );
 
     @Override
     public double evaluate(GameState gameState, int playerId) {
         double score = 0;
+        double openSquares = boardOpenness(gameState) / (double) BoardConstants.TOTAL_SIZE;
 
         // Score based on current score
         score += gameState.getScores()[playerId] - gameState.getScores()[1 - playerId];
 
-        // Score based on tiles in rack
+        // Score based on tiles in rack and how open the board is
         char[] myRack = gameState.getRacks()[playerId].getLetters();
         char[] opponentsRack = gameState.getRacks()[1 - playerId].getLetters();
+        int rackScore = 0;
         for (int i = 0; i < myRack.length; i++) {
-            score += TILE_UTILITIES.getOrDefault(myRack[i], 0) - TILE_UTILITIES.getOrDefault(opponentsRack[i], 0);
+            rackScore += TILE_UTILITIES.getOrDefault(myRack[i], 0) - TILE_UTILITIES.getOrDefault(opponentsRack[i], 0);
         }
+        // If the board is more open, having better tiles is better
+        rackScore += openSquares;
+        // Update score according to rack score
+        score += rackScore;
 
         return score;
+    }
+
+    private int boardOpenness(GameState gameState) {
+        int openSquares = 0;
+        Board board = gameState.getBoard();
+        for (int i = 0; i < BoardConstants.TOTAL_SIZE; i++) {
+            if (board.tileAt(i) == GameConstants.EMPTY_SQUARE) {
+                openSquares++;
+            }
+        }
+        return openSquares;
     }
 }
